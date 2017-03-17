d7e057f8abdd6604c9d6b9561f266dd5ca82169d
rojdes <rojdes@gmail.com>
2017-03-17 11:17:53 +0200

    Disable jack, add SystemEventsService

--- app/src/main/java/org/d_m_n/callspider/callspider/services/SystemEventsService.java
+++ app/src/main/java/org/d_m_n/callspider/callspider/services/SystemEventsService.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
-
+import org.d_m_n.callspider.callspider.app.Logger;
 import org.d_m_n.callspider.callspider.observers.ContactsContentObserver;
 
 /**
@@ -17,6 +17,7 @@ import org.d_m_n.callspider.callspider.observers.ContactsContentObserver;
 public final class SystemEventsService extends Service {
 
 
+    private static final String TAG = SystemEventsService.class.getSimpleName();
     private ContactsContentObserver mContactsObserver;
 
     @Nullable
@@ -27,13 +28,22 @@ public final class SystemEventsService extends Service {
 
     @Override
     public void onCreate() {
+        super.onCreate();
+        startForeground(0, null);
+        Logger.e(TAG, "onCreate");
         registerReceivers(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Logger.e(TAG, "onDestroy");
         unregisterReceivers(this);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
     }
 
     private void registerReceivers(Context ctxt) {
